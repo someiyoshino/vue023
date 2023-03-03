7f2fb2ff534cd7b970a264aad7e9909cedb80257
yoshida <yoshi-2020-qqh@jinmusoftware.com>
2023-03-03 20:48:48 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script setup>
+import TheHeader from './components/TheHeader.vue';
+import TheFooter from './components/TheFooter.vue';
+</script>
+
+<template>
+    <div class="wrap">
+        <TheHeader />
+        <main class="main">Main</main>
+        <TheFooter />
+    </div>
+</template>
+
+<style>
+@import 'assets/css/destyle.css';
+
+.wrap {
+    background-color: rgba(207, 221, 233, 0.3);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    min-height: 100vh;
+    width: 370px;
+    margin: 0 auto;
+    font-family: sans-serif;
+}
+.main {
+    background-color: rgba(207, 233, 223, 0.3);
+
+    flex: 1;
+    width: 100%;
+}
+</style>
